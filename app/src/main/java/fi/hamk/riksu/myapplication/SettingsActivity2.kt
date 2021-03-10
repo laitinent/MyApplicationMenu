5f34a6011e9c5992cb7fab27afe16ecd4c3b4854
Toni Laitinen <toni.laitinen@hamk.fi>
2021-03-10 10:56:52 +0200

    using fragment

---
+++ app/src/main/java/fi/hamk/riksu/myapplication/SettingsActivity2.kt
@@ -0,0 +1,25 @@
+package fi.hamk.riksu.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            //setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            addPreferencesFromResource(R.xml.pref_general)
+        }
+    }
+}
